14048224f3e7f798326840990676c7f7333933d9
lishangbu <shangshili@hotmail.com>
2026-02-04 10:45:04 +0800

    refactor: 拆分poke api

---
+++ avalon-support/avalon-pokeapi-support/avalon-pokeapi-exporter/src/main/java/io/github/lishangbu/avalon/pokeapi/model/TypeExcelDTO.java
@@ -0,0 +1,9 @@
+package io.github.lishangbu.avalon.pokeapi.model;
+
+/// 属性模型Excel数据传输对象
+///
+/// @param id                  资源标识符
+/// @param name                资源名称
+/// @author lishangbu
+/// @since 2026/2/4
+public record TypeExcelDTO(Integer id, String name) {}
