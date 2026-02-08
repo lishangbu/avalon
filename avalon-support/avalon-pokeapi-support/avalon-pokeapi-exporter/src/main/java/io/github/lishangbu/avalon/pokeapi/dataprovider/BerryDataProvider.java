47fae60e88d31105ccd93f942cb67d193d69e94f
lishangbu <shangshili@hotmail.com>
2026-02-08 13:18:12 +0800

    feat: add BerryDataProvider and BerryExcelDTO for berry data handling

---
+++ avalon-support/avalon-pokeapi-support/avalon-pokeapi-exporter/src/main/java/io/github/lishangbu/avalon/pokeapi/dataprovider/BerryDataProvider.java
@@ -0,0 +1,34 @@
+package io.github.lishangbu.avalon.pokeapi.dataprovider;
+
+import io.github.lishangbu.avalon.pokeapi.enumeration.PokeDataTypeEnum;
+import io.github.lishangbu.avalon.pokeapi.model.BerryExcelDTO;
+import io.github.lishangbu.avalon.pokeapi.model.berry.Berry;
+import io.github.lishangbu.avalon.pokeapi.model.item.Item;
+import io.github.lishangbu.avalon.pokeapi.util.NamedApiResourceUtils;
+import org.springframework.stereotype.Service;
+
+/// 树果数据提供者
+///
+/// @author lishangbu
+/// @since 2026/2/4
+@Service
+public class BerryDataProvider extends AbstractPokeApiDataProvider<Berry, BerryExcelDTO> {
+
+  @Override
+  public BerryExcelDTO convert(Berry berry) {
+    BerryExcelDTO result = new BerryExcelDTO();
+    result.setId(berry.id());
+    result.setInternalName(berry.name());
+    Item item = pokeApiService.getEntityFromUri(PokeDataTypeEnum.ITEM, NamedApiResourceUtils.getId(berry.item()));
+    result.setName(resolveLocalizedNameFromNames(item.names(), item.name()));
+    result.setGrowthTime(berry.growthTime());
+    result.setMaxHarvest(berry.maxHarvest());
+    result.setBulk(berry.size());
+    result.setSmoothness(berry.smoothness());
+    result.setSoilDryness(berry.soilDryness());
+    result.setFirmnessInternalName(berry.firmness().name());
+    result.setNaturalGiftTypeInternalName(berry.naturalGiftType().name());
+    result.setNaturalGiftPower(berry.naturalGiftPower());
+    return result;
+  }
+}
